cee2ace283dde49d21a129567212a6995b8dc3e4
ncsamii <ncsameie@gmail.com>
2024-02-26 14:58:25 +0330

    add required component

---
+++ src/app/components/column-manager/column-manager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-column-manager',
+  templateUrl: './column-manager.component.html',
+  styleUrl: './column-manager.component.scss'
+})
+export class ColumnManagerComponent {
+
+}
